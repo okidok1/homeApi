cca9e8936c4ab51f83d5587d4a9789603d69f8cb
Anastasia Saenko <asaenko@fil-it.ru>
2022-09-03 19:26:22 +0300

    first commit

---
+++ src/main/java/ru/homeapi/core/HomeApiApplication.java
@@ -0,0 +1,13 @@
+package ru.homeapi.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/homeapi/core/HomeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.homeapi.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
